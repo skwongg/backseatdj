f3c0a59fdfde133dfec8436cb333d2b392026084
skwongg <silaskwong1@gmail.com>
2015-06-25 23:00:06 -0700

    fixing git issue

--- app/models/playlist.rb
+++
@@ -1,5 +0,0 @@
-class Playlist < ActiveRecord::Base
-	belongs_to :user
-	has_many :songs, dependent: :destroy  # if playlist is deleted so are its songs
-
-end
--- app/models/song.rb
+++
@@ -1,4 +0,0 @@
-class Song < ActiveRecord::Base
-	belongs_to :playlist
-  validates_uniqueness_of :track_id, :scope => [:playlist_id]
-end
